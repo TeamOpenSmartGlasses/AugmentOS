a7f5b266f229d50988408883b844aeae3b78d018
Jeremy Stairs <jeremy@divergenceneuro.com>
2023-05-23 12:19:28 -0700

    LLM chat with memory

--- server/server.py
+++ server/server.py
@@ -17,6 +17,7 @@ from time import time
 
 app = web.Application()
 app['buffer'] = dict()
+app['jarvis memory'] = dict()
 # lower max token decreases latency: https://platform.openai.com/docs/guides/production-best-practices/improving-latencies. On average, each token is 4 characters.
 # Let's imagine the longest question someone is going to ask is 30 seconds and the longest reponse we'll get is 30 seconds
 # we speak 150 wpm
@@ -24,7 +25,8 @@ app['buffer'] = dict()
 max_talk_time = 30  # seconds
 # max_tokens = (((150 * (max_talk_time / 60)) * 4.7) / 4) * 2  # *2 for response
 max_tokens = 3200
-app['llm'] = ChatOpenAI(temperature=0.5, max_tokens=max_tokens, request_timeout=12, max_retries=0)
+app['llm'] = ChatOpenAI(
+    temperature=0.5, max_tokens=max_tokens, request_timeout=12, max_retries=0)
 app['buffer']['test'] = [
     {'text': 'old message be old yo', 'timestamp': time() - 60 * 70},
     {'text': 'I like butts', 'timestamp': time()},
@@ -32,6 +34,10 @@ app['buffer']['test'] = [
 ]
 app['buffer']['cayden'] = []
 app['buffer']['jeremy'] = []
+app['buffer']['test'] = []
+app['jarvis memory']['cayden'] = []
+app['jarvis memory']['jeremy'] = []
+app['jarvis memory']['test'] = []
 
 
 def get_text_in_past_n_minutes(obj_list, n):
@@ -91,7 +97,7 @@ async def is_topic_reminder_requested(text):
 
 async def summarize_chat_history(text):
     response = app['llm']([HumanMessage(
-        content=f"Please summarize the following text to a short string that is easy to parse very quickly and just gives the gist of what is said. Feel free to leave out filler words, like 'the' and 'a' if they aren't useful to human understanding of the abbreviated text. The summarized text should be no more than 14 words long, but I really would rather if it can be 10 or less. Please respond kindly. Here is the text to summarize:\n{text}")])
+        content=f"Please summarize the following text to a short string that is easy to parse very quickly and just gives the gist of what is said. Feel free to leave out filler words, like 'the' and 'a' if they aren't useful to human understanding of the abbreviated text. The summarized text should be no more than 18 words long, but I really would rather if it can be 14 or less. Please respond kindly. Here is the text to summarize:\n{text}")])
 
     summary = response.content
     return summary
@@ -118,14 +124,44 @@ async def summarize_if_requested(text, userId):
     return summary
 
 
-async def answer_question_to_jarvis(text):
-    if text.lower().find("jarvis") != -1:
-        response = app['llm']([
-            SystemMessage(content="You, Jarvis, are an expert in every field who ignores everything said to him that is not relevant to a question. When you answer a question please do so with fewer than 16 words. Please respond with kindness."),
-            HumanMessage(content=text)
-        ])
+async def answer_question_to_jarvis(text, userId):
+    """
+    Jarvis QA has memory.
+    Once a conversation has been started, all human and AI messages are recorded and context is maintained.
+    If human speaks while jarvis is thinking, jarvis stops thinking and starts thinking about the combined message.
+    """
+    memory = app['jarvis memory'][userId]
+    jarvis_mentioned = text.lower().find("jarvis") != -1
 
-        return response.content
+    # Say Jarvis to start or stop talking to Jarvis
+    if not memory and not jarvis_mentioned:
+        return
+    if memory and jarvis_mentioned:
+        memory.clear()
+        return
+
+    # if user talks before jarvis responds, replace previous jarvis request with a new longer one
+    if memory and memory[-1]['speaker'] == userId:
+        print('user spoke before jarvis')
+        text = memory[-1]['text'] + '\n' + text
+        memory[-1]['text'] = text
+
+    memory.append({'speaker': userId, 'text': text})
+    response = app['llm']([
+        SystemMessage(content="You, Jarvis, are an expert in every field who ignores everything said to him that is not relevant to a question. When you answer a question please do so with fewer than 16 words. Please respond with kindness."),
+        *[
+            HumanMessage(content=mem['text']) if mem['speaker'] == userId
+            else AIMessage(content=mem['text'])
+            for mem in memory
+        ],
+        HumanMessage(content=text)
+    ])
+    # If user spoke more while Jarvis was thinking, discard thought and wait for updated one
+    if memory[-1]['text'] != text:
+        return ''
+
+    memory.append({'speaker': 'jarvis', 'text': response.content})
+    return response.content
 
 
 async def chat_handler(request):
@@ -156,14 +192,20 @@ async def chat_handler(request):
     with open(f'{userId}.log', 'a') as f:
         f.write(str({'text': text, 'timestamp': timestamp}) + '\n')
 
-    # Look for questions and commands in the text (async / currently)
+    # when in Jarvis comms mode, ignore summaries and reminders.
     summary = ''
     answer = ''
     try:
-        summary, answer = await asyncio.gather(summarize_if_requested(text, userId), answer_question_to_jarvis(text))
+        jarvis_mode = app['jarvis memory'][userId] or text.lower().find("jarvis") != -1
+        if jarvis_mode:
+            answer = await answer_question_to_jarvis(text, userId)
+            response = answer
+        else:
+            summary = await summarize_if_requested(text, userId)
+            response = summary
     except Exception as e:
         print(e)
-        summary = 'openai is busy'
+        summary = 'open AI is busy'
 
     if not app['buffer'][userId]:
         app['buffer'][userId] = []
@@ -171,13 +213,6 @@ async def chat_handler(request):
 
     print('summary: ', summary)
     print('answer: ', answer)
-
-    # If question was to get a summary, give the summary instead of Jarvis' answer
-    response = ''
-    if summary:
-        response = summary
-    elif answer:
-        response = answer
 
     return web.Response(text=json.dumps({'message': response}), status=200)
 
