8910eddac1b6e243a52696061ff10d24128f2dda
cayden <cpierc3@uwo.ca>
2023-07-21 14:44:13 -0400

    move web frontend from Contextual Search Engine repo to this CSE repo (frontend written by cayden in private repo)

---
+++ web_frontend/src/components/Dictaphone.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+const Dictaphone = (props) => {
+    const { onTranscriptChanged } = props;
+    const endOfTranscript = useRef(null);
+
+    const { transcript, browserSupportsSpeechRecognition, browserSupportsContinuousListening } = useSpeechRecognition({ })
+
+    useEffect(() => {
+        onTranscriptChanged(transcript); //send transcript to parent
+        endOfTranscript.current.scrollIntoView({ behavior: "smooth", block: 'nearest', inline: 'start'  });
+    }, [transcript])
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
+    if (browserSupportsContinuousListening) {
+      SpeechRecognition.startListening({ continuous: true })
+    } else {
+      // Fallback behaviour
+    }
+
+  return (
+    <div className="scrollcontent">
+      <p>{transcript}</p>
+      <div ref={endOfTranscript}></div>
+    </div>
+  )
+
+}
+
+export default Dictaphone
