703f6824f30aabde2526be59ae4276b5cc7d8b7c
Alex Israelov <alex1115alex@gmail.com>
2023-07-31 19:45:38 -0600

    Implement server_config.py

---
+++ server/server_config.py
@@ -0,0 +1,12 @@
+gcp_project_id = "personal-337917"
+google_maps_api_key = ""
+
+# Uncomment one of the following for prod/dev:
+#
+# Prod:
+# databaseUri = "mongodb+srv://alis4887:usa@testcluster.yzrnimk.mongodb.net/?retryWrites=true&w=majority"
+# server_port = 8080
+#
+# Dev:
+# databaseUri = "mongodb+srv://alexanderisraelov:usa@testcluster.d4mvpr8.mongodb.net/?retryWrites=true&w=majority"
+# server_port = 8081
