1c472c31868c329e4842dc111199a289815b5e72
Alex Israelov <alex1115alex@gmail.com>
2023-09-11 16:14:03 -0500

    Small clean up

--- server/Summarizer.py
+++ server/Summarizer.py
@@ -4,32 +4,7 @@ import os
 import openai
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-class Summarizer:
-
-    def __init__(self, databaseHandler):
-        self.databaseHandler = databaseHandler
-
-    def summarize_entity(self, entity_description: str, chars_to_use=1250):
-        # Check cache for summary first
-        summary = self.databaseHandler.findCachedSummary(entity_description)
-        if summary: 
-            print("$$$ SUMMARY: FOUND CACHED SUMMARY")
-            return summary
-        
-        # Summary does not exist. Get it with OpenAI
-        print("$$$ SUMMARY: SUMMARIZING WITH OPENAI")
-        summary = self.summarize_entity_with_openai(entity_description, chars_to_use)
-        self.databaseHandler.saveCachedSummary(entity_description, summary)
-        return summary
-
-    def summarize_entity_with_openai(self, entity_description: str, chars_to_use=1250):
-            # shorten entity_description if too long
-            entity_description = entity_description[:min(
-                chars_to_use, len(entity_description))]
-
-            # make prompt
-            # like "the" and "a" if they aren't useful to human understanding
-            prompt = f"""Please summarize the following "entity description" text to 8 words or less, extracting the most important information about the entity. The summary should be easy to parse very quickly. Leave out filler words. Don't write the name of the entity. Use less than 8 words for the entire summary. Be concise, brief, and succinct.
+ogPrompt= """Please summarize the following "entity description" text to 8 words or less, extracting the most important information about the entity. The summary should be easy to parse very quickly. Leave out filler words. Don't write the name of the entity. Use less than 8 words for the entire summary. Be concise, brief, and succinct.
 
             Example:
             [INPUT]
@@ -44,7 +19,34 @@ class Summarizer:
             [OUTPUT]
             AI chatbot using GPT models.
 
-            \n Text to summarize: \n{entity_description} \nSummary (8 words or less): """
+            \n Text to summarize: \n{} \nSummary (8 words or less): """
+
+class Summarizer:
+
+    def __init__(self, databaseHandler):
+        self.databaseHandler = databaseHandler
+
+    def summarize_entity(self, entity_description: str, chars_to_use=1250):
+        # shorten entity_description if too long
+        entity_description = entity_description[:min(
+            chars_to_use, len(entity_description))]
+
+        # Check cache for summary first
+        summary = self.databaseHandler.findCachedSummary(entity_description)
+        if summary: 
+            print("$$$ SUMMARY: FOUND CACHED SUMMARY")
+            return summary
+        
+        # Summary does not exist. Get it with OpenAI
+        print("$$$ SUMMARY: SUMMARIZING WITH OPENAI")
+        summary = self.summarize_entity_with_openai(entity_description)
+        self.databaseHandler.saveCachedSummary(entity_description, summary)
+        return summary
+
+    def summarize_entity_with_openai(self, entity_description: str):
+            # make prompt
+            # like "the" and "a" if they aren't useful to human understanding
+            prompt = ogPrompt.format(entity_description)
             # print("Running prompt for summary: \n {}".format(prompt))
             chat_completion = openai.Completion.create(
                 model="text-davinci-002", prompt=prompt, temperature=0.5, max_tokens=20)
