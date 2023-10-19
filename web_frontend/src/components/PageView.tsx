713526534f3a2b46fb9a7941ff960ab04dcf96e1
Reece Yang <reeceyang@live.com>
2023-10-19 18:47:20 -0400

    close explore pane with button

--- web_frontend/src/components/PageView.tsx
+++ web_frontend/src/components/PageView.tsx
@@ -1,8 +1,6 @@
 import {
   ActionIcon,
-  Center,
   Skeleton,
-  Text,
   Tooltip,
   Flex,
   CloseButton,
@@ -10,6 +8,7 @@ import {
   Stack,
 } from "@mantine/core";
 import { IconArrowUp } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 interface PageViewProps {
   viewMoreUrl: string | undefined;
@@ -18,53 +17,57 @@ interface PageViewProps {
 }
 
 const PageView = ({ viewMoreUrl, loading, setLoading }: PageViewProps) => {
+  const [isHidden, setIsHidden] = useState(false);
+
   const handleLoad = () => {
     setLoading(false);
   };
+
+  useEffect(() => {
+    // reopen the explore pane when viewMoreUrl changes
+    setIsHidden(false)
+  }, [viewMoreUrl]);
+
+  if (isHidden) {
+    return null;
+  }
+
   return (
     <Flex sx={{ height: "100%" }}>
-      {viewMoreUrl ? (
-        <Skeleton
-          visible={loading}
-          h={"100%"}
-          w={"100%"}
-          sx={{ position: "relative" }}
-        >
-          <Tooltip label="Open page in browser">
-            <ActionIcon
-              component="a"
-              href={viewMoreUrl}
-              target="_blank"
-              variant="light"
-              color="indigo"
-              size="xl"
-              radius="xl"
-              sx={{ position: "absolute", right: 0, bottom: 0 }}
-            >
-              <IconArrowUp
-                style={{ width: "70%", height: "70%" }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-          </Tooltip>
-          <iframe
-            id="zoomed-out-iframe"
-            src={viewMoreUrl}
-            onLoad={handleLoad}
-            width="100%"
-            height="100%"
-            frameBorder={0}
-          ></iframe>
-        </Skeleton>
-      ) : (
-        <Center h={"100%"}>
-          <Text size={"xl"}>
-            Click on a reference card for more information
-          </Text>
-        </Center>
-      )}
-      <Stack align="center">
-        <CloseButton size={rem(25)} />
+      <Skeleton
+        visible={loading}
+        h={"100%"}
+        w={"100%"}
+        sx={{ position: "relative" }}
+      >
+        <Tooltip label="Open page in browser">
+          <ActionIcon
+            component="a"
+            href={viewMoreUrl}
+            target="_blank"
+            variant="light"
+            color="indigo"
+            size="xl"
+            radius="xl"
+            sx={{ position: "absolute", right: 0, bottom: 0 }}
+          >
+            <IconArrowUp
+              style={{ width: "70%", height: "70%" }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Tooltip>
+        <iframe
+          id="zoomed-out-iframe"
+          src={viewMoreUrl}
+          onLoad={handleLoad}
+          width="100%"
+          height="100%"
+          frameBorder={0}
+        ></iframe>
+      </Skeleton>
+      <Stack align="center" w="3rem">
+        <CloseButton onClick={() => setIsHidden(true)} size={rem(25)} mt="sm"/>
       </Stack>
     </Flex>
   );
