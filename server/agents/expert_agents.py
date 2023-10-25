bf64fbaa21aa8bd0fd83cb5f298e8d68ce6702be
KenjiPcx <kenji.phang@outlook.com>
2023-10-25 01:36:59 +0100

    Changed agent insight response to return an object with insight generation references

--- server/agents/expert_agents.py
+++ server/agents/expert_agents.py
@@ -17,46 +17,20 @@ agent = initialize_agent([
     ], llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, max_iterations=3, early_stopping_method="generate", verbose=True)
 
 
-def clean_expert_agent_output(expert_agent_response):
-    #for consistency, the final response starts with "Insight: ", let's remove that
-    if expert_agent_response == "null": 
-        expert_agent_response = None
-    else:
-        expert_agent_response = expert_agent_response[len("Insight:"):]
-        if expert_agent_response == "null": 
-            expert_agent_response = None
-    return expert_agent_response
-
-
 def run_single_expert_agent(expert_agent_name, convo_context, insights_history: list):
     #initialize the requested expert agent - using the name given
     expert_agent_config = expert_agent_config_list[expert_agent_name]
     #run the agent
-    expert_agent_response = expert_agent_run_wrapper(expert_agent_config, convo_context, insights_history)
-    #clean the output
-    expert_agent_response = clean_expert_agent_output(expert_agent_response)
-
-    return expert_agent_response
+    return expert_agent_run_wrapper(expert_agent_config, convo_context, insights_history)
 
 
 async def expert_agent_arun_wrapper(expert_agent_config, convo_context, insights_history: list):
-    #get agent response
-    expert_agent_response = await agent.arun(expert_agent_prompt_maker(expert_agent_config, convo_context, insights_history))
-    #clean the output
-    expert_agent_response = clean_expert_agent_output(expert_agent_response)
-    return {
-        "agent_name": expert_agent_config["agent_name"],
-        "agent_insight": expert_agent_response
-        } 
-
+    #run the agent
+    return await agent.arun(expert_agent_prompt_maker(expert_agent_config, convo_context, insights_history))
+  
 
 def expert_agent_run_wrapper(expert_agent_config, convo_context, insights_history: list):
-    #get agent response
-    expert_agent_response = agent.run(expert_agent_prompt_maker(expert_agent_config, convo_context, insights_history))
-    #clean the output
-    expert_agent_response = clean_expert_agent_output(expert_agent_response)
-    return {
-        "agent_name": expert_agent_config["agent_name"],
-        "agent_insight": expert_agent_response
-    } 
+    #run the agent
+    return agent.run(expert_agent_prompt_maker(expert_agent_config, convo_context, insights_history))
+
 
