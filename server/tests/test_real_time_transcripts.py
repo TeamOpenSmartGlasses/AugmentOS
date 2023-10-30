68816ff3bf1bd87e0c6018bdaefef71ab5bbf3b0
KenjiPcx <kenji.phang@outlook.com>
2023-10-30 01:13:05 +0000

    Added a script to test transcripts in real time

---
+++ server/tests/test_real_time_transcripts.py
@@ -0,0 +1,43 @@
+import time
+import webvtt
+from test_helper import *
+
+def test_transcripts_in_realtime(file_path):
+    captions = webvtt.read(file_path)
+    start_time = time.time()
+
+    for caption in captions:
+        start = timestamp_to_seconds(caption.start)
+        end = timestamp_to_seconds(caption.end)
+
+        # Wait until the start time
+        while time.time() - start_time < start:
+            time.sleep(0.01)
+
+        # Send transcript to server
+        print(caption.text)
+        test_on_text(caption.text)
+
+        # Wait until the end time
+        while time.time() - start_time < end:
+            time.sleep(0.01)
+
+        # Get the response from the server
+        ui_poll()
+
+def timestamp_to_seconds(timestamp):
+    parts = timestamp.split(":")
+    
+    if len(parts) == 3:  # format is "HH:MM:SS.sss"
+        hours, minutes, rest = parts
+    else:  # format is "MM:SS.sss"
+        hours = 0
+        minutes, rest = parts
+
+    seconds, milliseconds = rest.split(".")
+    total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) * 0.001
+    return total_seconds
+
+if __name__ == "__main__":
+    file_path = input("Enter the path to the VTT file: ")
+    test_transcripts_in_realtime(file_path)
