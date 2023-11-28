8bf12a350616d063d419d1adc28ed355c26d8e26
Reece Yang <reeceyang@live.com>
2023-11-28 10:43:29 -0500

    fix typescript theme issues

--- web_frontend/src/theme.ts
+++ web_frontend/src/theme.ts
@@ -3,13 +3,13 @@ import { ColorScheme } from "@mantine/core";
 export const theme = {
   colorScheme: "dark" as ColorScheme,
   colors: {
-    cardFill: "hsla(231, 28%, 13%, 1)",
-    titleText: "hsla(240, 31%, 87%, 1)",
-    bodyText: "hsla(240, 27%, 73%, 1)",
-    cardStroke: "hsla(227, 33%, 27%, 1)",
-    convoscopeBlue: "hsla(200, 99%, 53%, 1)",
-    iconColor: "hsla(227, 40%, 63%, 1)",
-  },
+    cardFill: ["hsla(231, 28%, 13%, 1)"],
+    titleText: ["hsla(240, 31%, 87%, 1)"],
+    bodyText: ["hsla(240, 27%, 73%, 1)"],
+    cardStroke: ["hsla(227, 33%, 27%, 1)"],
+    convoscopeBlue: ["hsla(200, 99%, 53%, 1)"],
+    iconColor: ["hsla(227, 40%, 63%, 1)"],
+  } as Record<string, [(string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?, (string | undefined)?]>,
   primaryColor: "convoscopeBlue",
 
   defaultGradient: {
