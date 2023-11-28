36e501a7574dccc3ba9663767b1b7d2fb1f79877
Alex Israelov <alex1115alex@gmail.com>
2023-11-27 23:58:54 -0600

    Add GPT4 capability, can also swap between stock/azure easily

--- server/Modules/LangchainSetup.py
+++ server/Modules/LangchainSetup.py
@@ -1,7 +1,17 @@
 from langchain.chat_models import ChatOpenAI
-from server_config import openai_api_key, azure_openai_api_key, azure_openai_api_base, azure_openai_api_deployment
+from langchain.chat_models.azure_openai import AzureChatOpenAI
+from server_config import use_azure_openai, openai_api_key, azure_openai_api_key, azure_openai_api_base, azure_openai_api_gpt4_deployment
 from langchain.chat_models.azure_openai import *
 from constants import GPT_4_MODEL, GPT_4_MAX_TOKENS, GPT_TEMPERATURE
 
 def get_langchain_gpt4(temperature=GPT_TEMPERATURE, model=GPT_4_MODEL, max_tokens=GPT_4_MAX_TOKENS):
-    return ChatOpenAI(temperature=temperature, openai_api_key=openai_api_key, model=model, max_tokens=max_tokens)
+    if use_azure_openai:
+        return AzureChatOpenAI(openai_api_key=azure_openai_api_key, 
+                            openai_api_base=azure_openai_api_base,
+                            openai_api_version = "2023-05-15",
+                            deployment_name=azure_openai_api_gpt4_deployment,
+                            temperature=temperature,
+                            max_tokens=max_tokens,
+                            )
+    else:
+        return ChatOpenAI(temperature=GPT_TEMPERATURE, openai_api_key=openai_api_key, model=model, max_tokens=GPT_4_MAX_TOKENS)
--- server/Modules/QueryLLM.py
+++ server/Modules/QueryLLM.py
@@ -1,17 +1,19 @@
 # OpenAI imports
 import openai
-from server_config import openai_api_key, use_azure_openai, azure_openai_api_key, azure_openai_api_base, azure_openai_api_deployment
+from server_config import openai_api_key, use_azure_openai, azure_openai_api_key, azure_openai_api_base, azure_openai_api_gpt35_deployment
 
 if use_azure_openai:
     openai.api_key = azure_openai_api_key
     openai.api_base = azure_openai_api_base # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
     openai.api_type = 'azure'
     openai.api_version = '2023-08-01-preview' # this may change in the future
-    deployment_name = azure_openai_api_deployment # This will correspond to the custom name you chose for your deployment when you deployed a model. 
+    deployment_name = azure_openai_api_gpt35_deployment # This will correspond to the custom name you chose for your deployment when you deployed a model. 
 else:
     openai.api_key = openai_api_key
 
 def one_off_query(prompt, max_tokens=30):
+    response = "Alex was here :)"
+
     if use_azure_openai:
         messages = [{"role": "user", "content": prompt}]
         chat_completion = openai.ChatCompletion.create(engine=deployment_name, messages=messages, max_tokens=max_tokens)
@@ -20,6 +22,14 @@ def one_off_query(prompt, max_tokens=30):
         else:
             response = chat_completion['choices'][0]['message']['content'].replace('\n', '').replace(' .', '.').strip()
     else:
-        response = "OpenAI placeholder"
+        messages = [{"role": "user", "content": prompt}]
+        chat_completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=messages,
+            max_tokens=max_tokens,
+            temperature=0,
+        )
+
+        response = chat_completion.choices[0].message["content"]
     
     return response
