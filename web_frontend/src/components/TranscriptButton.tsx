1d9a341c78086b0abd07055f1cb52edb4a6dae95
Reece Yang <reeceyang@live.com>
2023-11-28 10:41:12 -0500

    remove card styles from transcript button

--- web_frontend/src/components/TranscriptButton.tsx
+++ web_frontend/src/components/TranscriptButton.tsx
@@ -13,21 +13,19 @@ import { debounce } from "lodash";
 import axiosClient from "../axiosConfig";
 import { CHAT_ENDPOINT } from "../serverEndpoints";
 
-const useStyles = createStyles((theme) => ({
-  card: { backgroundColor: theme.white, borderRadius: "0.25rem" },
-
-  iconButton: {
-    width: "2rem",
-    height: "2rem",
-    animation: `${breathe} 6s ease-in-out infinite`
-  },
-}));
-
 const breathe = keyframes`
   0% { transform: translate(0); filter: brightness(1); }
   50% { transform: translate(0, -6px); filter: brightness(1.2); }
   100% { transform: translate(0); filter: brightness(1); }
 `;
+
+const useStyles = createStyles({
+  iconButton: {
+    width: "2rem",
+    height: "2rem",
+    animation: `${breathe} 6s ease-in-out infinite`,
+  },
+});
 
 const TranscriptButton = () => {
   const { classes } = useStyles();
