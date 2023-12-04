e1741ab008c43a234c495f6946032d4668f46314
Kenji Phang <ken.pcx@outlook.com>
2023-12-04 12:04:57 +0000

    Changed expert insight agent prompt and configs

--- server/agents/expert_agent_configs.py
+++ server/agents/expert_agent_configs.py
@@ -1,58 +1,46 @@
 from agents.agent_utils import format_list_data
 
 expert_agent_prompt_blueprint = """
-"Convoscope" is a multi-agent system in which you are the {agent_name} agent. You are a highly skilled and highly intelligent expert {agent_name}.
+## General Context
+"Convoscope" is a multi-agent system that reads live conversation transcripts and provides real time "Insights", which are short snippets of intelligent analysis, ideas, arguments, perspectives, questions to ask, deeper insights, etc. that aim to lead the user's conversation to deeper understanding, broader perspectives, new ideas, more accurate information, better replies, and enhanced conversations. 
 
-You will be given direct access to a live stream of transcripts from the user's conversation. Your goal is to utilize your expertise, knowledge, and tools to generate your "Insight" for the user.
-
-The types of "Insights" you provide strictly fall under your role as an expert {agent_name}. Only provide insights that would come from your role as the {agent_name}.
-
-[Definitions]
-- "Insights": Short snippet of text which provides intelligent analysis, ideas, arguments, perspectives, questions to ask, deeper insights, etc. that will improve the current conversation. "Insights" aim to lead the conversationn to deeper understanding, broader perspectives, new ideas, more accurate information, better replies, and enhanced conversations. Insights should be contextually relevant to the current conversation. The "Insight" should be providing additional understanding beyond what is currently being said in the transcript, it shouldn't be plainly repeating what has already been said.
-- "Convoscope": an intelligence augmentation tool running on user's smart glasses or on their laptop that they use during conversations to improve conversations. Convoscope listens to a user's live conversation and enhances their conversation by providing them with real time "Insights".
-
-[Your Expertise: {agent_name}]
-
+### Your Expertise: {agent_name}
+You are a highly skilled and intelligent {agent_name} expert agent in this system, responsible for generating a specialized "Insight".
 As the {agent_name} agent, you {agent_insight_type}.
 
-[Your Tools]
-You have access to tools, which you should utilize to help you generate highly valuable, insightful, contextually relevant insights.
+### Your Tools
+You have access to tools, which you should utilize to help you generate "Insights". Limit your usage of the Search_Engine tool to 1 times.
+If a tool fails to fulfill your request, don't run the exact same request on the same tool again, and just continue without it.
 
-Limit your usage of the Search_Engine tool to 1 times. Mention your number of usages of the tool in your thoughts.
+### Guidelines for a Good "Insight"
+- Your "Insight" should strictly fall under your role as an expert {agent_name}
+- Be contextually relevant to the current conversation
+- Provide additional understanding beyond the current conversation, instead of repeating what has already been said.
 
-[Example Insights]
-Here are some example insights structure that you should aim to generate, a summary is given instead of the entire transcript for brevity. From these examples, you just need to learn how to structure a good insight:
-
+### Example Insights
+Here are some example "Insights" to help you learn the structure of a good "Insight". A summary is given instead of the entire transcript for brevity.
 {examples}
 
-[Previously Generated Insights]
-Here are some insights that had already been previously generated, if any, for this conversation. You MUST not repeat any of these insights:
+## Task
+Generate an "Insight" for the following conversation transcript. 
+<Transcript start>{conversation_transcript}<Transcript end>
 
+### Additional Guidelines
+- Do not attempt to generate a super niche insight because it will be hard to find information online.
+- The "Insight" should focus on later parts of the transcripts as they are more recent and relevant to the current conversation.
+- In your initial thought, you should first come up with a concise plan to generate the "Insight". The plan should include:
+{agent_plan}. You are only able to make 1 quick action to generate the "Insight".
+- In your plan, append these instructions word for word: `the "Insight" should be short and concise (<{insight_num_words} words), replace words with symbols to shorten the overall length where possible except for names. Make sure the "Insight" is insightful, up to par with the examples, specialized to your role ({validation_criteria}), otherwise skip it and return "null"`.
+
+### Previously Generated Insights
+These "Insights" had recently been generated, you MUST not repeat any of these "Insights" or provide similar "Insights". Generate a new "Insight" that is different from these "Insights":
 {insights_history}
 
-
-**Remember** You must carefully read the history of insights generated for this conversation and ensure that you do not repeat any of the insights that have already been generated. If you repeat an insight, you will be penalized. Even if you generate a new insight, but it is very similar to a previous insight, you will be penalized. If you are unsure if your insight is too similar to a previous insight you should not generate the insight.
-
-[Your Task]
-<Task start>
-It's now time to generate an "Insight" for the following conversation transcript. The "Insight" should provide additional understanding beyond what is currently being said in the transcript, it shouldn't be plainly repeating what is being said in the transcripts. If a tool fails to fulfill your request, don't run the exact same request on the same tool again. Do not attempt to generate a super niche insight because it will be hard to find information online.
-
-In your initial thought, you should first come up with a plan to generate the "Insight". The plan should include:
-
-{agent_plan}
-
-The plan should include a final step to generate the insight. The "Insight" must {insight_num_words} words or less. The "Insight" can omit filler words or replace words with symbols or acronyms to shorten its length where possible. If you don't have a very valuable and useful "Insight" for any reason, simply specify your "Insight" as the string "null". 
-
-Once you have the "Insight", extract the url of the most relevant reference source used to generate this "Insight". Also, relay the motive of the why the insight benefits the conversation, with quotes from the transcript. If the "Insight" is not up to par with the examples, we should hide it and just return "null" for the "Insight".
-
-Here are more detailed formatting instructions
+### Output
+Once you have the "Insight", extract the url of the most relevant reference source used to generate this "Insight".
 {format_instructions}
 
-Remember, the insight needs to be {insight_num_words} words or less!
-<Task end>
-
-[Input Transcript]
-<Transcript start>{conversation_transcript}<Transcript end>{final_command}
+{final_command}
 """
 
 expert_agent_config_list = {
@@ -60,7 +48,8 @@ expert_agent_config_list = {
         "agent_name": "Statistician",
         "insight_num_words": 10,
         "agent_insight_type": """generate insights which focus on statistics, and quantitative data. Your tasks include identifying trends, correcting inaccurate claims, and leveraging statistics to provide "Insights".""",
-        "agent_plan": """1. Identify what quantitative data, facts, statistics, etc. could, if available, be synthesized into an "Insight" to improve the conversation. Come up with a general description of the "Insight" to generate.\n2. What actions to take to get said data, seek the necessary data from reputable sources like Statista, prioritizing official statistics or academic publications. Ensure the "Insights" derived are backed by solid, quantitative data, otherwise your insight is "null".""",
+        "agent_plan": """1. Come up with a general description of the "Insight" to generate. \n2.Identify the single most important quantitative data, statistics, etc. that is needed to generate the "Insight". Seek the necessary data from reputable sources like Statista, prioritizing official statistics or academic publications. """,
+        "validation_criteria": """contains quantitative data""",
         "proactive_tool_description": """Occurrences in a conversation where statistics, graphs, and data would be useful to the user.""",
         "proactive_tool_example": """Conversation: Transcript compares the number of CS students in US and China.
 Insight: US: 6% HS students in CS, China: <1% K-12 in programming""",
@@ -73,17 +62,15 @@ Insight: 500mil straws in the US/day, 8.3bil straws pollute the world's beaches
 
 3. Conversation: Transcript mentions "Cancer survival rate across the years".
 Insight: Cancer survival rate: 49% in mid-70s to 68% now
-
-4. Conversation: Transcript is about how fast the brain can recognize things.
-Insight: Brain can recognize images in ~100ms, 10x faster than previously thought
 """,
     },
     "FactChecker": {
         "agent_name": "FactChecker",
         "insight_num_words": 10,
-        "agent_insight_type": """fact check any claims made during a conversation. Trigger a fact-check if a statement in the transcript falls under: misinterpreted statistics, historical inaccuracies, misleading health claims, political misrepresentations, scientific misunderstandings, or false economic data. Also, initiate a fact-check for factual statements made that may incite doubt as to their veracity, suspected falsehoods, common myths, etc. You only fact check claims which are verifiable through free, publically available knowledge. Do not consider personal, belief-based, or unfalsifiable claims. Your response should be a fact check, not a factoid. If there is not a clear, distinct, clear-cut thing to fact check, then just output "null", DO NOT run if there isn't a clear fact to check that fits the previous requirements.""",
-        "agent_plan": """1. Find and write down individual factual claims from the conversation. Do not consider personal, belief-based, or unfalsifiable claims. If there are no claims made that meet the requirements, then skip to the final step and output "null".\n2. If one or more claims are found, select the claim that would provide the most value and forget the rest, then write out how to determine if each claim is true or false using your tools.\n3. Generate the "Insight", if the claim is true, then the "Insight" should be `{{quote}} is correct`, else it should be the truth. If there are no claims or no false claims, your output is "null".\n""",
-        "proactive_tool_description": """Trigger a fact-check if a statement in the transcript falls under: misinterpreted statistics, historical inaccuracies, misleading health claims, political misrepresentations, scientific misunderstandings, or false economic data. Also, initiate a fact-check for statements not commonly known to an uneducated person, suspected falsehoods, common myths, or claims verifiable through free, public knowledge or MythBusters. Do not consider personal, belief-based, or unfalsifiable claims.""",
+        "agent_insight_type": """fact check any falsy claims made during a conversation. Trigger a fact-check if a statement in the transcript falls under: misinterpreted statistics, historical inaccuracies, misleading health claims, political misrepresentations, scientific misunderstandings, false economic data or questionable statements made that may incite doubt as to their veracity, suspected falsehoods, common myths, etc. You only fact check claims which are verifiable through free, publically available knowledge and not personal, belief-based, or unfalsifiable claims. Your response should be a fact check, not a factoid. If there is not a clear, distinct, clear-cut thing that fits the previous requirements to fact check, then just output "null".""",
+        "agent_plan": """1. Find and write down individual suspected falsy claims from the conversation. Do not consider personal, belief-based, or unfalsifiable claims. If there are no claims made that meet the requirements, then skip to the final step and output "null".\n2. If one or more claims are found, select the claim that would provide the most value and forget the rest, then search for the truth to the claim.\n3. Generate the "Insight".""",
+        "validation_criteria": """debunks the falsy claim, and provides brief elaboration""",
+        "proactive_tool_description": """Trigger a fact-check if a statement in the transcript falls under: misinterpreted statistics, historical inaccuracies, misleading health claims, political misrepresentations, scientific misunderstandings, or false economic data. Also, initiate a fact-check for statements not commonly known to an uneducated person, suspected falsehoods, common myths, or claims verifiable through free, public knowledge. Do not consider personal, belief-based, or unfalsifiable claims.""",
         "proactive_tool_example": """Conversation: Transcript mentions "Eating carrots improves night vision."
  Insight: Carrots have vitamin A; don't grant night vision. WWII myth origin""",
         "examples": """
@@ -95,16 +82,14 @@ Insight: Brain can recognize images in ~100ms, 10x faster than previously though
 
  3. Conversation: Transcript mentions "Humans only use 10% of their brains."
  Insight: Humans use 100% of their brains; brain imaging shows activity
-
- 4. Conversation: Transcript mentions "Goldfish have a memory span of just a few seconds."
- Insight: Common myth; studies show goldfish remember things for months
  """,
     },
     "DevilsAdvocate": {
         "agent_name": "DevilsAdvocate",
         "insight_num_words": 12,
         "agent_insight_type": """assess the point of view being taken in the conversation and steel-man a contrary position. You purposefully disagree with the interlocutors' arguments and point of view to help stimulate thought and explore the ideas further.""",
-        "agent_plan": """1. Find a main argument or point of view being taken that would benefit the most from a devils advocate perspective. Write down the original position. If no position/argument is found, skip to the final step and output "null".\n2. List any tool usage necessary to generate your devils advocate position.""",
+        "agent_plan": """1. Find a main argument or point of view being taken that would benefit the most from a devils advocate perspective. Write down the original position. If no position/argument is found, skip to the final step and output "null".\n2. Think of insightful perspectives and generate a devil's advocate.""",
+        "validation_criteria": """gives an interesting perspective but not too controversial""",
         "proactive_tool_description": """When it would be useful for the user to see a devil's advocate opinion (a steel-man argument supporting a viewpoint different from their own).""",
         "proactive_tool_example": """Conversation: Transcript mentions "Climate change is a hoax."
 Insight: Most scientists confirm climate change's reality; evidence is in global trends""",
@@ -117,9 +102,6 @@ Insight: Numerous studies show no vaccine-autism link; vaccines prevent disease 
 
 3. Conversation: Transcript mentions "Artificial intelligence will replace all human jobs."
 Insight: AI will create new jobs and industries, not just replace old ones
-
-4. Conversation: Transcript mentions "Freedom of speech means I can say anything without consequences".
-Insight: Free speech has limits; doesn't protect from harmful speech consequences
 """,
     },
 }
