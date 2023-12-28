1838b91b8fb3b27a53f313543c99150f6ca449fe
Alex Israelov <alex1115alex@gmail.com>
2023-12-27 19:05:36 -0600

    Add record_on_lex script

--- server/tests/test_helper.py
+++ server/tests/test_helper.py
@@ -7,12 +7,21 @@ import multiprocessing
 TEST_USERID = "caydenLexTester"
 TEST_DEVICEID = "testDeviceId"
 
+# TEST_USERID = "testUserId"
+# TEST_DEVICEID = "CSEWebFrontendDefault"
+
 server_endpoint = '/dev2'
 URL = "https://vpmkebx0cl.execute-api.us-east-2.amazonaws.com/api"
+
+# server_endpoint = ''
+# URL = "http://localhost:8080"
 
 URI = URL + server_endpoint
 UI_POLL_ENDPOINT = URI + "/ui_poll"
 CHAT_ENDPOINT = URI + "/chat"
+START_RECORDING_ENDPOINT = URI + "/start_recording"
+SAVE_RECORDING_ENDPOINT = URI + "/save_recording"
+LOAD_RECORDING_ENDPOINT = URI + "/load_recording"
 PLAYBACK_SPEED = 2
 
 ui_poll_data = {
@@ -34,6 +43,15 @@ def ui_poll_loop():
         ui_poll()
         time.sleep(5)
 
+# Like `ui_poll_loop`, but ends when there are no results after `timeout` seconds
+def ui_poll_loop_with_timeout(timeout = 60):
+    print(f"=== Begin UI Polling w/ {timeout} second timeout ===")
+    latest_result_time = time.time()
+    while time.time() < latest_result_time + timeout:
+        results = ui_poll()
+        if results['result']: latest_result_time = time.time()
+        time.sleep(5)
+
 def chat(text, isFinal):
     chat_data = {
       'text': text,
@@ -52,6 +70,27 @@ def test_on_text(text: str):
     chat(text, False)
     time.sleep(0.5)
     chat(text, True)
+
+def start_recording():
+    data = {'userId': TEST_USERID}
+    r = requests.post(START_RECORDING_ENDPOINT, data=json.dumps(data))
+    r_json = r.json()
+    print(r_json)
+    return r_json
+
+def save_recording(recording_name: str = "testRecording"):
+    data = {'userId': TEST_USERID, 'recordingName': recording_name}
+    r = requests.post(SAVE_RECORDING_ENDPOINT, data=json.dumps(data))
+    r_json = r.json()
+    print(r_json)
+    return r_json
+
+def load_recording(recording_name: str = "testRecording"):
+    data = {'recordingName': recording_name}
+    r = requests.post(LOAD_RECORDING_ENDPOINT, data=json.dumps(data))
+    r_json = r.json()
+    print(r_json)
+    return r_json
     
 if __name__ == "__main__":
     test_on_text("Hey Convoscope, do cars generally have 4 wheels?")
