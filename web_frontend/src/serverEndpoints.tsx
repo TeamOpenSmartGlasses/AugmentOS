3f1c4557c6c55261b4ca9caeecab4025f9808302
Reece Yang <reeceyang@live.com>
2024-01-30 00:13:02 -0500

    add server endpoint to frontend

--- web_frontend/src/serverEndpoints.tsx
+++ web_frontend/src/serverEndpoints.tsx
@@ -1,7 +1,7 @@
 //modify the url to point to the correct endpoint
 const serverEndpointEnv = import.meta.env.VITE_SERVER_ENVIRONMENT;
-const isProd = (serverEndpointEnv === "prod"); //check if we're running on prod
-const baseEndpoint = isProd ? "/api" : ("/api/" + serverEndpointEnv); //if prod, no extra endpoint location, otherwise append the endpoint name
+const isProd = serverEndpointEnv === "prod"; //check if we're running on prod
+const baseEndpoint = isProd ? "/api" : "/api/" + serverEndpointEnv; //if prod, no extra endpoint location, otherwise append the endpoint name
 
 export const UI_POLL_ENDPOINT = baseEndpoint + "/ui_poll";
 
@@ -13,4 +13,7 @@ export const AGENT_RUN_ENDPOINT = baseEndpoint + "/run_single_agent";
 
 export const SEND_AGENT_CHAT_ENDPOINT = baseEndpoint + "/send_agent_chat";
 
-export const RATE_INSIGHT_ENDPOINT = baseEndpoint + "/rate_result"
+export const RATE_INSIGHT_ENDPOINT = baseEndpoint + "/rate_result";
+
+export const SUMMARIZE_CONVERSATION_ENDPOINT =
+  baseEndpoint + "/summarize_conversation";
