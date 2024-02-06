9c67e1946ae12d1fa100be465d8a036f24bf9484
cayden <cpierc3@uwo.ca>
2024-02-06 03:09:38 +0000

    fix sc

--- server/server_config.py
+++ server/server_config.py
@@ -1,17 +1,16 @@
-gcp_project_id = "alexwearllm"
-google_maps_api_key = "AIzaSyAZwPNSVy5zo98R35Rt8bG44MrDIQQdOcs"
-serper_api_key = "e1102f25d8a807b8b18e7aa03482122ae4a55c68"
-openai_api_key = "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"
-wolframalpha_api_key = "U7P33Q-9K7J7V6AG2"
+gcp_project_id = ""
+google_maps_api_key = ""
+serper_api_key = ""
+openai_api_key = ""
+wolframalpha_api_key = ""
 clear_users_on_start = True
 clear_cache_on_start = False
 
 use_azure_openai = True
-azure_openai_api_key = "2b52199c5da945ec952b0753c4027e23"
+azure_openai_api_key = ""
 azure_openai_api_base = "https://convoscoperesource2.openai.azure.com/"
 azure_openai_api_gpt35_deployment = "ConvoscopeGPT3"
 azure_openai_api_gpt4_deployment = "ConvoscopeGPT4"
-
 
 time_everything_spreadsheet_id = "1UD_Lf4V9yP5fdpgNb1PwLJAye2A7K5ihw7ozoO3cmz4"
 
