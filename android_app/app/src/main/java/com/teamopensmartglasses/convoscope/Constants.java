cbe5d11d775cd7df4c7e7f95d946660e19d40ea7
cayden <cpierc3@uwo.ca>
2024-02-18 20:12:35 -0500

    (part 2) turn off Ultralite automatic screen turn off, add Hebrew to Google ASR, + make the UI poll way faster

--- android_app/app/src/main/java/com/teamopensmartglasses/convoscope/Constants.java
+++ android_app/app/src/main/java/com/teamopensmartglasses/convoscope/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     //endpoints
     public static final String LLM_QUERY_ENDPOINT = "/chat";
     public static final String BUTTON_EVENT_ENDPOINT = "/button_event";
-    public static final String CSE_ENDPOINT = "/ui_poll";
+    public static final String UI_POLL_ENDPOINT = "/ui_poll";
     public static final String SET_USER_SETTINGS_ENDPOINT = "/set_user_settings";
 }
 
