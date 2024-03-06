d3db211b8f5546efbd29370eb2d7969bad254ec0
Alex Israelov <alex1115alex@gmail.com>
2024-03-06 13:49:52 -0600

    add llama to LangchainSetup

--- server/Modules/LangchainSetup.py
+++ server/Modules/LangchainSetup.py
@@ -1,11 +1,24 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.chat_models.azureml_endpoint import *
 from langchain.chat_models.azure_openai import AzureChatOpenAI
-from server_config import use_azure_openai, openai_api_key, azure_openai_api_key, azure_openai_api_base, azure_openai_api_gpt4_deployment, azure_openai_api_gpt35_deployment, azure_openai_api_gpt35_16k_deployment
+from langchain.chat_models.azureml_endpoint import AzureMLChatOnlineEndpoint
+from server_config import use_azure_openai, openai_api_key, azure_openai_api_key, azure_openai_api_base, azure_openai_api_gpt4_deployment, azure_openai_api_gpt35_deployment, azure_openai_api_gpt35_16k_deployment, azure_api_llama_13b_chat_endpoint, azure_api_llama_13b_chat_api_key
 
 from langchain.chat_models.azure_openai import *
 from constants import GPT_4_MODEL, GPT_4_MAX_TOKENS, GPT_TEMPERATURE, GPT_35_MODEL, GPT_35_MAX_TOKENS, GPT_35_16K_MODEL, GPT_35_16K_MAX_TOKENS
 
-from langchain.callbacks import openai_info
+from langchain_community.callbacks import openai_info
+
+def get_langchain_tiny():
+    formatter = LlamaChatContentFormatter()
+
+    return AzureMLChatOnlineEndpoint(
+        endpoint_url=azure_api_llama_13b_chat_endpoint,
+        endpoint_api_type='serverless',
+        endpoint_api_key=azure_api_llama_13b_chat_api_key,
+        content_formatter=formatter,
+        model_kwargs={"temperature": 0.8, "max_new_tokens": 400}
+    )
 
 def get_langchain_gpt4(temperature=GPT_TEMPERATURE, model=GPT_4_MODEL, max_tokens=GPT_4_MAX_TOKENS):
     if use_azure_openai:
