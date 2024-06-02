5d50436e78defd92b520eb8bfca5ebfed7ff91da
Alex Israelov <alex1115alex@gmail.com>
2024-06-02 14:15:21 -0500

    Auth fixes

--- android_app/app/src/main/java/com/teamopensmartglasses/convoscope/TokenHelper.java
+++ android_app/app/src/main/java/com/teamopensmartglasses/convoscope/TokenHelper.java
@@ -1,5 +1,7 @@
 package com.teamopensmartglasses.convoscope;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,10 +25,17 @@ public class TokenHelper {
     public static void getToken(TokenListener listener) throws JSONException {
         // Set a buffer time of 5 minutes before expiration
         long bufferTime = 5 * 60 * 1000; // 5 minutes in milliseconds
+        long currentTime = System.currentTimeMillis();
 
-        if (cachedToken != null && (System.currentTimeMillis() + bufferTime) < tokenExpirationTime) {
+        Log.d("TokenHelper", "Current Time: " + currentTime);
+        Log.d("TokenHelper", "Token Expiration Time: " + tokenExpirationTime);
+        Log.d("TokenHelper", "Time Left: " + (tokenExpirationTime - currentTime));
+
+        if (cachedToken != null && (currentTime + bufferTime) < tokenExpirationTime) {
+            Log.d("TokenHelper", "Using cached token.");
             listener.onTokenReceived(cachedToken);
         } else {
+            Log.d("TokenHelper", "Fetching new token.");
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             if (user != null) {
                 user.getIdToken(true).addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
@@ -35,18 +44,22 @@ public class TokenHelper {
                         if (task.isSuccessful()) {
                             String idToken = task.getResult().getToken();
                             cachedToken = idToken;
-                            tokenExpirationTime = System.currentTimeMillis() + (task.getResult().getExpirationTimestamp() * 1000);
+                            tokenExpirationTime = (task.getResult().getExpirationTimestamp() * 1000);
+                            Log.d("TokenHelper", "New Token: " + cachedToken);
+                            Log.d("TokenHelper", "New Token Expiration Time: " + tokenExpirationTime);
                             try {
                                 listener.onTokenReceived(idToken);
                             } catch (JSONException e) {
                                 throw new RuntimeException(e);
                             }
                         } else {
+                            Log.e("TokenHelper", "Failed to get new token.", task.getException());
                             listener.onTokenFailed(task.getException());
                         }
                     }
                 });
             } else {
+                Log.e("TokenHelper", "User not authenticated.");
                 listener.onTokenFailed(new Exception("User not authenticated"));
             }
         }
