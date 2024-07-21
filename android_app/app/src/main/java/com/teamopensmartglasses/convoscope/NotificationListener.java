dc7f037fdb9ab9fa2604d7865da4053defa4f4ba
Alex Israelov <alex1115alex@gmail.com>
2024-07-21 14:12:19 -0500

    Fix bugs

--- android_app/app/src/main/java/com/teamopensmartglasses/convoscope/NotificationListener.java
+++ android_app/app/src/main/java/com/teamopensmartglasses/convoscope/NotificationListener.java
@@ -1,18 +1,11 @@
 package com.teamopensmartglasses.convoscope;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
-
-import androidx.core.app.NotificationCompat;
 
 import com.teamopensmartglasses.convoscope.events.NewScreenTextEvent;
 
@@ -24,8 +17,6 @@ import java.util.List;
 public class NotificationListener extends NotificationListenerService {
     private static final String TAG = "NotificationListener";
 
-    private static final String CHANNEL_ID = "NotificationRelayChannel";
-
     private final List<String> packageBlacklist = Arrays.asList(
             "com.android.systemui",
             "com.samsung.android.app.smartcapture"
@@ -34,43 +25,12 @@ public class NotificationListener extends NotificationListenerService {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "NotificationService onCreate");
-        startForegroundService();
+        Log.d(TAG, "Service created");
     }
 
-    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
-    }
-
-    private void startForegroundService() {
-        createNotificationChannel();
-
-//        Intent notificationIntent = new Intent(this, MainActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        Notification notification = new Notification.Builder(this, CHANNEL_ID)
-//                .setContentTitle("Notification Relay Service")
-//                .setContentText("Capturing incoming notifications")
-//                .setSmallIcon(com.teamopensmartglasses.smartglassesmanager.R.drawable.elon)
-//                .setContentIntent(pendingIntent)
-//                .build();
-        Notification notification = createNotification();
-        startForeground(3591, notification);
-    }
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel serviceChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Notification Relay Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-            );
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(serviceChannel);
-        }
-
-        Log.d(TAG, "Started notification listener");
+        Log.d(TAG, "Service Started");
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -118,15 +78,15 @@ public class NotificationListener extends NotificationListenerService {
         Log.d(TAG, "Stopped notification listener");
     }
 
-    private Notification createNotification() {
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+        Log.d(TAG, "Listener Connected");
+    }
 
-        return new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Notification Listener Service")
-                .setContentText("Capturing notifications")
-                .setSmallIcon(com.teamopensmartglasses.smartglassesmanager.R.drawable.elon)
-                .setContentIntent(pendingIntent)
-                .build();
+    @Override
+    public void onListenerDisconnected() {
+        super.onListenerDisconnected();
+        Log.d(TAG, "Listener Disconnected");
     }
 }
