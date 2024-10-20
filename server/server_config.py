c6928a2903f647ed013002c31197b53b5fb2e877
cayden <cpierc3@uwo.ca>
2024-10-20 17:12:17 -0500

    Remove some old LLM endpoints & api key

--- server/server_config.py
+++ server/server_config.py
@@ -5,16 +5,11 @@ use_azure_openai = True
 openai_api_key = ""
 
 # Populate this if using Azure OpenAI API
-azure_openai_api_key = "8fb4dd771c3047cbb33821b96464c18c"
+azure_openai_api_key = ""
 azure_openai_api_base = "https://convoscoperesource2.openai.azure.com/"
 azure_openai_api_gpt35_deployment = "ConvoscopeGPT3"
-azure_openai_api_gpt35_16k_deployment = "ConvoscopeGPT3-16K"
 azure_openai_api_gpt4_deployment = "ConvoscopeGPT4"
 azure_openai_api_gpt4o_deployment = "ConvoscopeGPT4o"
-
-# Unused for now
-azure_api_llama_13b_chat_endpoint = "https://Llama-2-13b-chat-aypss-serverless.eastus2.inference.ai.azure.com/v1/chat/completions"
-azure_api_llama_13b_chat_api_key = "HwPgqg72o5eUepbIcMJldrkn7fOtZ6t9"
 
 time_everything_spreadsheet_id = "1UD_Lf4V9yP5fdpgNb1PwLJAye2A7K5ihw7ozoO3cmz4"
 
@@ -27,7 +22,7 @@ ignore_auth = False
 gcp_project_id = "alexwearllm"
 google_maps_api_key = "AIzaSyDrLB_P3dXj-CktMkNrHzlLS7p5cAZS2_Q"
 serper_api_key = "0313f26c9142afce8e6c09d296aef3f3649d2361"
-wolframalpha_api_key = "U7P33Q-WLYJUA7GWA"
+wolframalpha_api_key = "8QUE3U-WXEW9AATHW" 
 
 # Uncomment one of the following configs:
 # Local:
