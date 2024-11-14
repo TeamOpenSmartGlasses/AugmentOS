9f6bf06ed047fd938885516e5e03e828a5e5f3ce
Dillon Devera <dillondevera12@gmail.com>
2024-11-14 01:15:41 -0700

    Add notification listener functionality and related permissions

---
+++ AugmentOS_Manager/android/app/src/main/java/com/augmentos_manager/NotificationListenerService.kt
@@ -0,0 +1,24 @@
+package com.augmentos_manager
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.content.Intent
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
+class NotificationListenerService : NotificationListenerService() {
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val packageName = sbn.packageName
+        val notificationText = sbn.notification.extras.getString("android.text")
+
+        // Send the notification data to React Native via a broadcast
+        val intent = Intent("NOTIFICATION_LISTENER")
+        intent.putExtra("package", packageName)
+        intent.putExtra("text", notificationText)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        // Optional: handle notification removal
+    }
+}
