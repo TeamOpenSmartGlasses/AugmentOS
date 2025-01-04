ab42422bdc01cd3fa83c9b4a28e840ce2c06f6d4
Nicolo Micheletti <michelettinik@gmail.com>
2025-01-04 15:14:45 -0800

    Fix ReferenceCard Problem

--- augmentos_android_library/AugmentOSLib/src/main/java/com/teamopensmartglasses/augmentoslib/events/ReferenceCardSimpleViewRequestEvent.java
+++ augmentos_android_library/AugmentOSLib/src/main/java/com/teamopensmartglasses/augmentoslib/events/ReferenceCardSimpleViewRequestEvent.java
@@ -5,18 +5,19 @@ import java.io.Serializable;
 public class ReferenceCardSimpleViewRequestEvent implements Serializable {
     public String title;
     public String body;
-    public int lingerTimeMs;
+    public int lingerTimeMs = 6; // Default linger time
     public static final String eventId = "referenceCardSimpleViewRequestEvent";
 
     public ReferenceCardSimpleViewRequestEvent(String title, String body) {
         this.title = title;
         this.body = body;
-        this.lingerTimeMs = 0;
     }
 
     public ReferenceCardSimpleViewRequestEvent(String title, String body, int lingerTimeMs) {
         this.title = title;
         this.body = body;
-        this.lingerTimeMs = lingerTimeMs;
+        if (lingerTimeMs != 0) { // Leave default value if 0
+            this.lingerTimeMs = lingerTimeMs;
+        }
     }
 }
