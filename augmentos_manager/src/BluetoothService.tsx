7292314420fcc58d2971c1e42f0fb07e8554481a
Nicolo Micheletti <michelettinik@gmail.com>
2025-02-04 20:17:45 -0800

    Decrease heartbeat interval

--- augmentos_manager/src/BluetoothService.tsx
+++ augmentos_manager/src/BluetoothService.tsx
@@ -249,7 +249,7 @@ export class BluetoothService extends EventEmitter {
         //this.sendHeartbeat();
         this.reconnectionTimer = setTimeout(
           performScan,
-          this.connectedDevice ? 5000 : 5000,
+          this.connectedDevice ? 4000 : 4000,
         );
       }
 
