b845914eb917d525d196e20add72a564f5affa6f
Alex Israelov <alex1115alex@gmail.com>
2025-02-21 15:07:51 -0800

    add mentra live config 2

--- augmentos_core/app/src/main/java/com/augmentos/augmentos_core/smarterglassesmanager/supportedglasses/SmartGlassesOperatingSystem.java
+++ augmentos_core/app/src/main/java/com/augmentos/augmentos_core/smarterglassesmanager/supportedglasses/SmartGlassesOperatingSystem.java
@@ -2,6 +2,7 @@ package com.augmentos.augmentos_core.smarterglassesmanager.supportedglasses;
 
 public enum SmartGlassesOperatingSystem {
     ANDROID_OS_GLASSES,
+    SELF_OS_GLASSES,
     ACTIVELOOK_OS_GLASSES,
     ULTRALITE_MCU_OS_GLASSES,
     EVEN_REALITIES_G1_MCU_OS_GLASSES,
