6fcfba75ffbdaf8a3a68e76472d21c454464fc5b
cayden <cpierc3@uwo.ca>
2025-02-23 21:53:33 -0800

    fix some SGC G1 issues

--- augmentos_core/app/src/main/java/com/augmentos/augmentos_core/smarterglassesmanager/smartglassescommunicators/EvenRealitiesG1SGC.java
+++ augmentos_core/app/src/main/java/com/augmentos/augmentos_core/smarterglassesmanager/smartglassescommunicators/EvenRealitiesG1SGC.java
@@ -109,7 +109,7 @@ public class EvenRealitiesG1SGC extends SmartGlassesCommunicator {
     private static final long DELAY_BETWEEN_SENDS_MS = 8; //not using now
     private static final long DELAY_BETWEEN_CHUNKS_SEND = 16; //super small just in case
     private static final long DELAY_BETWEEN_ACTIONS_SEND = 250; //not using now
-    private static final long HEARTBEAT_INTERVAL_MS = 30000;
+    private static final long HEARTBEAT_INTERVAL_MS = 15000;
     private static final long MICBEAT_INTERVAL_MS = (1000 * 60) * 30; //micbeat every 30 minutes
 
     private int leftReconnectAttempts = 0;
@@ -288,10 +288,16 @@ public class EvenRealitiesG1SGC extends SmartGlassesCommunicator {
                     if ("Left".equals(side)) {
                         isLeftConnected = false;
                         leftReconnectAttempts++;
+                        if (leftGlassGatt != null){
+                            leftGlassGatt.close();
+                        }
                         leftGlassGatt = null;
                     } else {
                         isRightConnected = false;
                         rightReconnectAttempts++;
+                        if (rightGlassGatt != null){
+                            rightGlassGatt.close();
+                        }
                         rightGlassGatt = null;
                     }
 
@@ -541,7 +547,7 @@ public class EvenRealitiesG1SGC extends SmartGlassesCommunicator {
                 sendWhiteListCommand(10);
 
                 //start heartbeat
-                startHeartbeat(20000);
+                startHeartbeat(10000);
 
                 //start mic beat
                 startMicBeat(30000);
